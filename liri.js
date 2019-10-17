fc4b5fba9fffa27de46cbbaeb2092b1de5be84d3
alleykatwill <48265377+alleykatwill@users.noreply.github.com>
2019-10-16 22:00:43 -0400

    updated liri.js file

--- liri.js
+++ liri.js
@@ -1,204 +1,74 @@
-// Required packages and Vars
-
+//Read and set any environment variables with the .env package.
 require("dotenv").config();
-var request = require("request");
-var axios = require("axios");
-var keys = require("./keys.js");
-var Spotify = require(node - spotify - api);
-var spotify = new Spotify(keys.spotify);
+
+//Require data from File System npm package
 var fs = require("fs");
-var moment = require("moment");
 
-// user inputs
-var userOption = process.argv[2];
-var inputParameter = process.argv[3];
+// Requiring our Spotify function exported from spotify.js
+var mySpotify = require("./spotify.js");
+// Requiring our Movies function exported from omdb.js
+var myMovies = require("./movies.js");
+// Requiring our Movies function exported from omdb.js
+var myConcert = require("./concerts.js");
 
-// /
+//Creates initial user command.
+var userCommand = process.argv[2];
+//Creates user input.
+var userInput = process.argv.splice(3, process.argv.length).join(" ");
 
-//Execute function
-UserInputs(userOption, inputParameter);
-
-//FUNCTIONS
-function UserInputs(userOption, inputParameter) {
-  switch (userOption) {
-    case "concert-this":
-      showConcertInfo(inputParameter);
-      break;
-    case "spotify-this-song":
-      showSongInfo(inputParameter);
-      break;
-    case "movie-this":
-      showMovieInfo(inputParameter);
-      break;
-    case "do-what-it-says":
-      showSomeInfo();
-      break;
-    default:
-      console.log(
-        "Ruh Roh, An Error occurred. Please type any of the following options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says"
-      );
-  }
-}
-
-//Funtion for Concert Info: Bands in Town
-function showConcertInfo(inputParameter) {
-  var queryUrl =
-    "https://rest.bandsintown.com/artists/" +
-    inputParameter +
-    "/events?app_id=codingbootcamp";
-  request(queryUrl, function(error, response, body) {
-    // If the request is successful
-    if (!error && response.statusCode === 200) {
-      var concerts = JSON.parse(body);
-      for (var i = 0; i < concerts.length; i++) {
-        console.log("**********EVENT INFO*********");
-        fs.appendFileSync("log.txt", "**********EVENT INFO*********\n"); //Append in log.txt file
-        console.log(i);
-        fs.appendFileSync("log.txt", i + "\n");
-        console.log("Name of the Venue: " + concerts[i].venue.name);
-        fs.appendFileSync(
-          "log.txt",
-          "Name of the Venue: " + concerts[i].venue.name + "\n"
-        );
-        console.log("Venue Location: " + concerts[i].venue.city);
-        fs.appendFileSync(
-          "log.txt",
-          "Venue Location: " + concerts[i].venue.city + "\n"
-        );
-        console.log("Date of the Event: " + concerts[i].datetime);
-        fs.appendFileSync(
-          "log.txt",
-          "Date of the Event: " + concerts[i].datetime + "\n"
-        );
-        console.log("*****************************");
-        fs.appendFileSync("log.txt", "*****************************" + "\n");
-      }
-    } else {
-      console.log("Ruh Roh, An Error occurred.");
-    }
-  });
-}
-
-//Funtion for Music Info: Spotify
-function showSongInfo(inputParameter) {
-  if (inputParameter === undefined) {
-    inputParameter = "Get Jiggy With It"; //default Song
-  }
-  spotify.search(
-    {
-      type: "track",
-      query: inputParameter
-    },
-    function(err, data) {
-      if (err) {
-        console.log("Ruh Roh, An Error occurred: " + err);
-        return;
-      }
-      var songs = data.tracks.items;
-
-      for (var i = 0; i < songs.length; i++) {
-        console.log("**********SONG INFO*********");
-        fs.appendFileSync("log.txt", "**********SONG INFO*********\n");
-        console.log(i);
-        fs.appendFileSync("log.txt", i + "\n");
-        console.log("Song name: " + songs[i].name);
-        fs.appendFileSync("log.txt", "song name: " + songs[i].name + "\n");
-        console.log("Preview song: " + songs[i].preview_url);
-        fs.appendFileSync(
-          "log.txt",
-          "preview song: " + songs[i].preview_url + "\n"
-        );
-        console.log("Album: " + songs[i].album.name);
-        fs.appendFileSync("log.txt", "album: " + songs[i].album.name + "\n");
-        console.log("Artist(s): " + songs[i].artists[0].name);
-        fs.appendFileSync(
-          "log.txt",
-          "artist(s): " + songs[i].artists[0].name + "\n"
-        );
-        console.log("*****************************");
-        fs.appendFileSync("log.txt", "*****************************\n");
-      }
-    }
-  );
-}
-
-//Funtion for Movie Info: OMDB
-function showMovieInfo(inputParameter) {
-  if (inputParameter === undefined) {
-    inputParameter = "Mr. Nobody";
-    console.log("-----------------------");
-    fs.appendFileSync("log.txt", "-----------------------\n");
+//Program conditions
+switch (userCommand) {
+  // help function to clarify commands used
+  case "help":
     console.log(
-      "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/"
+      "Please type one of these commands\n" +
+        "'concert-this': to search your favorite artist concerts\n" +
+        "'spotify-this-song': to search your favorite song\n" +
+        "'movie-this': to search your favorite movie \n" +
+        "'do-what-it-says': using command from random.txt \n"
     );
-    fs.appendFileSync(
-      "log.txt",
-      "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/" +
-        "\n"
+    break;
+  case "concert-this":
+    myConcert(userInput);
+    break;
+  case "spotify-this-song":
+    mySpotify(userInput);
+    break;
+  case "movie-this":
+    myMovies(userInput);
+    break;
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
+  //if anything else written
+  default:
+    console.log(
+      "LIRI doesn't understand that - Please type 'node liri.js help' for more information"
     );
-    console.log("It's on Netflix!");
-    fs.appendFileSync("log.txt", "It's on Netflix!\n");
-  }
-  var queryUrl =
-    "http://www.omdbapi.com/?t=" +
-    inputParameter +
-    "&y=&plot=short&apikey=b3c0b435";
-  request(queryUrl, function(error, response, body) {
-    // If the request is successful
-    if (!error && response.statusCode === 200) {
-      var movies = JSON.parse(body);
-      console.log("**********MOVIE INFO*********");
-      fs.appendFileSync("log.txt", "**********MOVIE INFO*********\n");
-      console.log("Title: " + movies.Title);
-      fs.appendFileSync("log.txt", "Title: " + movies.Title + "\n");
-      console.log("Release Year: " + movies.Year);
-      fs.appendFileSync("log.txt", "Release Year: " + movies.Year + "\n");
-      console.log("IMDB Rating: " + movies.imdbRating);
-      fs.appendFileSync("log.txt", "IMDB Rating: " + movies.imdbRating + "\n");
-      console.log(
-        "Rotten Tomatoes Rating: " + getRottenTomatoesRatingValue(movies)
-      );
-      fs.appendFileSync(
-        "log.txt",
-        "Rotten Tomatoes Rating: " + getRottenTomatoesRatingValue(movies) + "\n"
-      );
-      console.log("Country of Production: " + movies.Country);
-      fs.appendFileSync(
-        "log.txt",
-        "Country of Production: " + movies.Country + "\n"
-      );
-      console.log("Language: " + movies.Language);
-      fs.appendFileSync("log.txt", "Language: " + movies.Language + "\n");
-      console.log("Plot: " + movies.Plot);
-      fs.appendFileSync("log.txt", "Plot: " + movies.Plot + "\n");
-      console.log("Actors: " + movies.Actors);
-      fs.appendFileSync("log.txt", "Actors: " + movies.Actors + "\n");
-      console.log("*****************************");
-      fs.appendFileSync("log.txt", "*****************************\n");
-    } else {
-      console.log("Ruh Roh, An Error occurred.");
+}
+
+function doWhatItSays() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    //Return error if error occurs.
+    if (error) {
+      return console.log(error);
     }
-  });
-}
-
-//Rotten Tomatoes Rating Function
-function getRottenTomatoesRatingObject(data) {
-  return data.Ratings.find(function(item) {
-    return item.Source === "Rotten Tomatoes";
-  });
-}
-
-function getRottenTomatoesRatingValue(data) {
-  return getRottenTomatoesRatingObject(data).Value;
-}
-
-//function for reading out of random.txt file
-function showSomeInfo() {
-  fs.readFile("random.txt", "utf8", function(err, data) {
-    if (err) {
-      return console.log(err);
-    }
+    // Then split it by commas (to make it more readable)
     var dataArr = data.split(",");
-    UserInputs(dataArr[0], dataArr[1]);
+
+    // Each command is represented. Because of the format in the txt file, remove the quotes to run these commands.
+    if (dataArr[0] === "spotify-this-song") {
+      var songcheck = dataArr[1].slice(1, -1);
+      console.log("Song Check: " + songcheck);
+      mySpotify(songcheck);
+    } else if (dataArr[0] === "concert-this") {
+      var venueName = dataArr[1].slice(1, -1);
+      console.log("Venue Name: " + venueName);
+      myConcert(venueName);
+    } else if (dataArr[0] === "movie-this") {
+      var movieName = dataArr[1].slice(1, -1);
+      console.log("Movie Name: " + movieName);
+      myMovies(movieName);
+    }
   });
 }
